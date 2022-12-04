fbc1b4c54f7d0e536e708ef901965bbcc50cc0dc
HarrisonBrookesBoon <harrison.m.brookes@gmail.com>
2022-12-04 18:19:37 +0000

    Add instructions screen

--- run.py
+++ run.py
@@ -7,10 +7,12 @@ import colorama
 import os
 
 
+f = Figlet(font="slant")
+
 def start_menu():
-    f = Figlet(font='slant')
-    os.system('clear')
-    print(f.renderText('Into the Depths'))
+    
+    os.system("clear")
+    print(f.renderText("Into the Depths"))
     print("Please choose an option by typing either '1' or '2' and pressing enter. \n \n")
     print("1. Start Game")
     print("2. Instructions")
@@ -31,6 +33,37 @@ def start_game():
     print("Game started!")
 
 def display_instructions():
-    print("Instructions:")
+    os.system("clear")
+    print(f.renderText("Instructions") + "\n \n")
+    print("Once you have started the game by choosing option '1' on the start menu,")
+    print("you will be placed into the starting room where you can choose from a small")
+    print("set of starting gear. If you are not happy with the choices presented, you")
+    print("may re-roll for different options. Otherwise, make your choice and enter the")
+    print("door to begin! \n")
+
+    print("You will now battle your way through rooms which present harder and more")
+    print("numerous enemies the further you go - finding more powerful items along the")
+    print("way. The goal is to survive as long as you can and build up the highest")
+    print("score possible. \n \n")
+
+    input("Press enter for the game controls")
+
+    os.system("clear")
+    print(f.renderText("Controls") + "\n") 
+    print("Movement:")
+    print("W - Up")
+    print("A - Left")
+    print("S - Down")
+    print("D - Right \n")
+    print("Attack:")
+    print("Up arrow - Attack upwards")
+    print("Left arrow - Attack left")
+    print("Down arrow - Attack down")
+    print("Right arrow - Attack right \n")
+    print("Miscellaneous:")
+    print("'i' - View inventory \n")
+
+    print(f.renderText("Good luck!"))
+
 
 start_menu()
